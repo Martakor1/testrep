b18cfc8a5f2f421c8ecf5f3576ad49b6693fe07e
Андрей Глазков <glazkoff.andrej2014@yandex.ru>
2021-12-26 00:14:00 +0300

    Изменил тест

--- tests/avlsettest.py
+++ tests/avlsettest.py
@@ -1,4 +1,6 @@
 import unittest
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
 import os
 
 def uploadInput(amount, inputStr):
@@ -22,4 +24,9 @@ class TestSum(unittest.TestCase):
         result = downloadOutput()
         self.assertEqual(result, expectedOut)
 
-unittest.main()
+if __name__ == '__main__':
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
