f7b82d88486790932d1ceaacc85deeca39f59129
Андрей Глазков <glazkoff.andrej2014@yandex.ru>
2021-12-26 13:05:12 +0300

    Добавил два теста

--- tests/test_avlsettest.py
+++ tests/test_avlsettest.py
@@ -23,6 +23,14 @@ class TestSum(unittest.TestCase):
         os.startfile("avlset.exe")
         result = downloadOutput()
         self.assertEqual(result, expectedOut)
+    def test2(self):
+        uploadInput(5, "A 3 A 2 A 7 A 8 A -5")
+        os.startfile("avlset.exe")
+        self.assertEqual("0 -1 0 1 0", downloadOutput())
+    def test3(self):
+        uploadInput(7, "A 3 A 2 D 3 A 6 A 8 C 6 C 3")
+        os.startfile("avlset.exe")
+        self.assertEqual("0 -1 0 1 0 Y N", downloadOutput())
 
 if __name__ == '__main__':
     if is_running_under_teamcity():
