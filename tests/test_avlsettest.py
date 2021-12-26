f4e11e1876f03f4cacb1c96bbe3fcd5ab0af6846
Андрей Глазков <glazkoff.andrej2014@yandex.ru>
2021-12-26 13:26:33 +0300

    Добавил subprocess

--- tests/test_avlsettest.py
+++ tests/test_avlsettest.py
@@ -1,7 +1,7 @@
 import unittest
 from teamcity import is_running_under_teamcity
 from teamcity.unittestpy import TeamcityTestRunner
-import os
+import subprocess
 
 def uploadInput(amount, inputStr):
     f = open("avlset.in", "w")
@@ -20,16 +20,16 @@ class TestSum(unittest.TestCase):
         inputStr = "A 3 A 4 A 5 C 4 C 6 D 5"
         expectedOut = "0 1 0 Y N -1"
         uploadInput(amount, inputStr)
-        os.startfile("avlset.exe")
+        subprocess.Popen("avlset.exe").wait()
         result = downloadOutput()
         self.assertEqual(result, expectedOut)
     def test2(self):
         uploadInput(5, "A 3 A 2 A 7 A 8 A -5")
-        os.startfile("avlset.exe")
+        subprocess.Popen("avlset.exe").wait()
         self.assertEqual("0 -1 0 1 0", downloadOutput())
     def test3(self):
         uploadInput(7, "A 3 A 2 D 3 A 6 A 8 C 6 C 3")
-        os.startfile("avlset.exe")
+        subprocess.Popen("avlset.exe").wait()
         self.assertEqual("0 -1 0 1 0 Y N", downloadOutput())
 
 if __name__ == '__main__':
